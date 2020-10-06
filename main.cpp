ff2d4aa5ba35ddacf7a381ccedc6014dd8502231
Cristóbal Escamilla <cristobalescamilla2000@gmail.com>
2020-10-06 10:21:04 -0500

    Crear función cargaRegistros

--- main.cpp
+++ main.cpp
@@ -1,10 +1,31 @@
 #include <iostream>
 #include <fstream>
 #include <string>
-#include "DoubleLinkedList.h"
-
 
 using namespace std;
+
+#include "DoubleLinkedList.h"
+#include "Registro.h"
+
+// Cargar los registros de el archivo .txt a un vector de apuntadores de tipo Registro con sus respectivos atributos
+// Complejidad: O(n)
+void cargaRegistros(DoubleLinkedList &lista){
+    string mes;
+    int dia;
+    string hora;
+    string direccionIP;
+    string razon;
+    
+    ifstream archivo("bitacora.txt");
+    
+    while(archivo >> mes >> dia >> hora >> direccionIP){
+        getline(archivo, razon);
+        lista.addLast(new Registro(mes, dia, hora, direccionIP, razon));
+    }
+    
+    archivo.close();  
+}
+
 
 int main() {
 
