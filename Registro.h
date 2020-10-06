c212130b854226b9350f2771ca72eb29304b3a95
Cristóbal Escamilla <cristobalescamilla2000@gmail.com>
2020-10-06 10:13:11 -0500

    Create Registro.h

---
+++ Registro.h
@@ -0,0 +1,36 @@
+/*
+ * Programación de estructuras de datos y algoritmos fundamentales
+ * Clase Registro implementada en Actividad 2.3
+ * 
+ * Equipo 13
+ * 
+ * Fernando Doddoli Lankenau - A00827038
+ * Mauricio Eugenio Zambrano Díaz - A00827055
+ * Cristóbal Alberto Escamilla Sada - A00827074
+ * 
+ * 10 de octubre del 2020
+ */
+
+#ifndef Registro_h
+#define Registro_h
+
+class Registro{
+private:
+    string mes;
+    int dia;
+    string hora;
+    string direccionIP;
+    string razon;
+public:
+    Registro(string, int, string, string, string);
+};
+
+Registro::Registro(string mes, int dia, string hora, string direccionIP, string razon){
+    this->mes = mes;
+    this->dia = dia;
+    this->hora = hora;
+    this->direccionIP = direccionIP;
+    this->razon = razon;
+}
+
+#endif
